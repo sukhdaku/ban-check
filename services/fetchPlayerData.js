9ba074faab9690569db233e74fa5e62c6548a4a9
Sukh Daku <amprince9465@gmail.com>
2025-07-21 20:55:51 +0530

    Update fetchPlayerData.js

--- services/fetchPlayerData.js
+++ services/fetchPlayerData.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 async function fetchPlayerData(uid) {
   try {
     const response = await axios.get(`https://asyncban.vercel.app/info?uid=${uid}`);
-    if (!response.data || !response.data.data || !response.data.data.player_info) {
+    if (!response.data || !response.data.playerData) {
       throw new Error('ID NOT FOUND');
     }
     const playerInfo = response.data.data.player_info;
